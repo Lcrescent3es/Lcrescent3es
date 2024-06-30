6aa3122989ebcb45caa0ccfa53a1fabbfa43cbb9
Jade.choe <112841705+Lcrescent3es@users.noreply.github.com>
2024-07-01 02:10:03 +0900

    Create bj2491.cpp

---
+++ codetest/baekjoon/bj2491/bj2491.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> nums;
+int N, answer, cnt = 1;
+int main(){
+    ios::sync_with_stdio(0), cin.tie(0);
+    cin >> N;
+    nums = vector<int>(N);
+
+    for (int& num : nums){
+        cin >> num;
+    }
+    for (int i = 0; i < N - 1; ++i) {
+        if (nums[i] <= nums[i + 1]) {
+            cnt++;
+        }
+        else{
+            answer = max(answer, cnt);
+            cnt = 1;
+        }
+    }
+    answer = max(answer, cnt);
+    cnt = 1;
+
+    for (int i = 0; i < N - 1; ++i) {
+        if (nums[i] >= nums[i + 1]) {
+            ++cnt;
+        }
+        else {
+            answer = max(answer, cnt);
+            cnt = 1;
+        }
+    }
+    answer = max(answer, cnt);
+
+    cout << answer;
+    return 0;
+}
